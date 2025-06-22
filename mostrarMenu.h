53032f1f53330397c7077a8867d1a32ffea58443
Fernando270617 <107822824+Fernando270617@users.noreply.github.com>
2025-06-22 13:43:56 -0500

    mostrarMenu.h

---
+++ mostrarMenu.h
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+void mostrarMenu() {
+    printf("\n=== SISTEMA CONTAMINACION ===                                                   \n");
+    printf("1. Agregar zona                                                                 \n");
+    printf("2. Registrar datos                                                              \n");
+    printf("3. Ver estado actual                                                            \n");
+    printf("4. Generar predicciones                                                         \n");
+    printf("5. Calcular promedios                                                           \n");
+    printf("6. Ver recomendaciones                                                          \n");
+    printf("7. Guardar datos                                                                \n");
+    printf("8. Cargar datos                                                                 \n");
+    printf("9. Salir                                                                        \n");
+    printf("Opcion: ");
+}
